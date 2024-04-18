c836d6f5f2787d069b5e9d8bff2323da869112e3
Richke2005 <richardke18@gmail.com>
2024-04-18 19:19:08 -0300

    alteracao nos exercicios de java

---
+++ desenv-orientado-obj/atividade_4/Main.java
@@ -0,0 +1,8 @@
+import classes.Teclado;
+
+public class Main{
+    public static void main(String[] args){
+        Teclado t1 = new Teclado(21, "lenovo", "preto");
+        t1.imprimeTeclado();
+    }
+}
---
+++ desenv-orientado-obj/atividade_4/classes/Teclado.java
@@ -0,0 +1,49 @@
+package classes;
+
+public class Teclado {
+    private int numSerie;
+    private String fabricante;
+    private String cor;
+
+    public Teclado(int numSerie, String fabricante, String cor){
+        this.numSerie = numSerie;
+        this.fabricante = fabricante;
+        this.cor = cor;
+    }
+
+    Teclado(){
+        this.numSerie = 0;
+        this.fabricante = null;
+        this.cor = null;
+    }
+
+    public int getNumSerie(){
+        return numSerie;
+    }
+
+    public void setNumSerie(int numSerie){
+        this.numSerie = numSerie;
+    }
+
+    public String getFabricante(){
+        return fabricante;
+    }
+
+    public void setFabricante(String fabricante){
+        this.fabricante = fabricante;
+    }
+
+    public void setCor(String cor){
+        this.cor = cor;
+    }
+
+    public String getCor(){
+        return cor;
+    }
+
+    public void imprimeTeclado(){
+        System.out.println(numSerie);
+        System.out.println(fabricante);
+        System.out.println(cor);
+    }
+}
