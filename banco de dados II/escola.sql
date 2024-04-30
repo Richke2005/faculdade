df448b2ec6ef7ff1cf0940149a13c1033fe0a3c9
Richke2005 <richardke18@gmail.com>
2024-04-29 21:01:19 -0300

    add database updated

--- banco de dados II/escola.sql
+++ banco de dados II/escola.sql
@@ -78,21 +78,6 @@ FOREIGN KEY (turma_key) REFERENCES turma(id_turma),
 PRIMARY KEY(aluno_key, turma_key)
 );
 
--- CREATE TABLE administracao(
--- id_adm INT AUTO_INCREMENT,
--- nome VARCHAR(40),
--- email VARCHAR(50),
--- PRIMARY KEY(id_adm)
--- );
-
--- CREATE TABLE cargo(
--- id_cargo INT AUTO_INCREMENT,
--- nome_cargo VARCHAR(30),
--- adm_key INT NOT NULL,
--- FOREIGN KEY (adm_key) REFERENCES administracao(id_adm),
--- PRIMARY KEY(id_cargo)
--- );
-
 
 /* insert disciplinas*/
 INSERT INTO `escola`.`disciplina` (`nome`, `ementa`, `cargaH`) VALUES ('Banco de dados II', 'Consultas avançadas em linguagem de consulta estruturada (SQL). Conceitos de banco de dados que proporcionam mais performance no trabalho com o banco: visões, gatilhos e procedimentos armazenados. Processamento de transações. Cursores. Funções. Análise e representação de dados.', '80');
@@ -117,6 +102,8 @@ INSERT INTO `escola`.`atividade` (`titulo`, `descricao`, `turma_key`) VALUES ('A
 /* insert sexo*/
 INSERT INTO `escola`.`sexo` (`sexo`) VALUES ('M');
 INSERT INTO `escola`.`sexo` (`sexo`) VALUES ('Masculino');
+INSERT INTO `escola`.`sexo` (`sexo`) VALUES ('F');
+INSERT INTO `escola`.`sexo` (`sexo`) VALUES ('Feminino');
 
 /* insert aluno*/
 INSERT INTO `escola`.`aluno` (`nome`, `email`, `idade`, `sexo`, `primeira_graduacao`, `est_civil`) VALUES ('richard', 'richardke18@gmail.com', '2005-06-22', 1, "S","S" );
@@ -125,19 +112,6 @@ INSERT INTO `escola`.`aluno` (`nome`, `email`, `idade`, `sexo`, `primeira_gradua
 /* insert aluno_turma*/
 INSERT INTO `escola`.`aluno_turma` (`aluno_key`, `turma_key`) VALUES ('1', '1');
 INSERT INTO `escola`.`aluno_turma` (`aluno_key`, `turma_key`) VALUES ('2', '1');
-
-/* insert administracao*/
--- INSERT INTO `escola`.`administracao` (`nome`, `email`) VALUES ('Raquel', 'raquel@hotmail.com');
--- INSERT INTO `escola`.`administracao` (`nome`, `email`) VALUES ('Hebert', 'Hebert@hotamail.com');
-
-/* insert cargo*/
--- INSERT INTO `escola`.`cargo` (`nome_cargo`, `adm_key`) VALUES ('Diretor', '2');
--- INSERT INTO `escola`.`cargo` (`nome_cargo`, `adm_key`) VALUES ('Secretaria', '1');
-
-
--- Procedure:
-
--- procedure que retorna todos os alunos matriculados em uma determinada turma:
 
 DELIMITER $$
 CREATE PROCEDURE listar_alunos_por_turma(IN turma_id INT)
@@ -166,6 +140,21 @@ DELIMITER $$
 DELIMITER ;
 
 CALL  listar_professores_por_disciplina(2);
+
+DELIMITER $$
+    CREATE PROCEDURE qtd_by_sexo(IN sexo VARCHAR(10))
+    BEGIN
+        DECLARE qtd INT;
+
+        SELECT COUNT(sexo.sexo) INTO qtd FROM aluno
+        INNER JOIN sexo ON aluno.sexo = sexo.id_sexo WHERE sexo.sexo = sexo;
+
+    SELECT qts as "Quantidade de alunos";
+
+    END $$
+DELIMITER ;
+
+CALL qtd_by_sexo("Masculino");
 
 -- Função:
 
@@ -216,10 +205,9 @@ DELIMITER ;
 
 -- Consulta que faz uso de uma união de tabelas para recuperar informações de aluno_turma, turma e disciplina.
 
-
 SELECT aluno.nome AS nome_aluno, turma.id_turma, disciplina.nome AS nome_disciplina
 FROM aluno
-INNER JOIN aluno_turma ON aluno.id_aluno = aluno_turma.aluno_key
+INNER JOIN aluno_turma ON  aluno_turma.aluno_key = aluno.id_aluno
 INNER JOIN turma ON aluno_turma.turma_key = turma.id_turma
 INNER JOIN disciplina ON turma.disciplina_key = disciplina.id_disciplina;
 
