aeba89f472be03771d70c6a54ce98f5c2ef2fbf3
Richard ´C Dos Anjos <richardke18@gmail.com>
2024-05-09 17:11:44 +0000

    add ex10

---
+++ algoritmos-est-dados/vetores-e-matrizes/ex10.c
@@ -0,0 +1,26 @@
+// Leia uma matriz 4 x 4, conte e escreva quantos valores maiores que 10 ela possui.
+#include <stdio.h>
+
+int main(void){
+    int matriz[4][4],soma = 0 i, j;
+    
+    for(i = 0; i < 4; i++){
+        for(j = 0; j < 4; j++){
+            printf("Digite um valor para matriz[%d][%d]\n", i + 1, j + 1);
+            scanf("%d", &matriz[i][j]);
+        }
+    } 
+
+    for(i = 0; i < 4; i++){
+        for(j = 0; j < 4; j++){
+            if(matriz[i][j] > 10){
+                printf("Matriz[%d][%d] = %d  Maior que 10\n",i, j,matriz[i][j]);
+                soma++;
+            }
+        }
+    }
+
+    printf("Ha %d valores maiores que 10", soma);
+
+    return 0;
+}
