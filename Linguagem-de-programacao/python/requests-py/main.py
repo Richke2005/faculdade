b2f87fb5d712f628a022fd39cf5566b3a3b53b8f
Richard ´C Dos Anjos <90470694+Richke2005@users.noreply.github.com>
2024-06-16 22:44:26 -0300

    Add files via upload
    interact with whatsapp API from meta

--- Linguagem-de-programacao/python/requests-py/main.py
+++ Linguagem-de-programacao/python/requests-py/main.py
@@ -1,40 +1,53 @@
-from fastapi import FastAPI, Query
 import requests
 
-app = FastAPI()
+def send_whatsapp_message(my_number_code: str, to: str, message: str) -> dict:
+    """
+    Sends a WhatsApp message to the specified recipient.
 
-@app.get("/api")
-def read_root():
-    '''
+    Args:
+    my_number_code (str): The Facebook ID of the sender's WhatsApp number.
+    to (str): The recipient's WhatsApp number in the format '55' + country code + phone number.
+    message (str): The text message to be sent.
 
-    Endpoint que exibe uma menssagem incrível do mundo da programação
+    Returns:
+    dict: A dictionary containing the response from the WhatsApp API.
 
-    '''
-    return {"message": "Hello World"}
+    Raises:
+    requests.exceptions.HTTPError: If the API request encounters an HTTP error.
 
+    Example:
+    ```
+    response = send_whatsapp_message('286165347924405', '11947316281', 'Hello, this is a test message.')
+    print(response)
+    ```
+    """
+    url = f'https://graph.facebook.com/v19.0/{my_number_code}/messages'
+    body = {
+        "messaging_product": "whatsapp",
+        "recipient_type": "individual",
+        "to": f"55{to}",
+        "type": "text",
+        "text": {
+            "preview_url": False,
+            "body": message
+        }
+    }
+    headers = {
+        "Authorization": "Bearer EAAOJU1FBXfcBOxzJfshs5RxS6JrZAOcsuLCEhwZCKRTR4kI9QYwufP5ZCBA2RToxZAiaKtCZBodZBRV9v3XYc51bPXbIen9XUayjRyexCgBE5JqodvGoYZCZClPF3EF04ctEjfqt1wG24IIhZA4CsafvfrbUM2GMgT3YJ9uQHm7ayxusZAWBvHJMozuEuA4zTRZBQuqAK2U1VYfz5mAceLDvZCUZD"
+    }
+    response = requests.post(url, json=body, headers=headers)
 
-@app.get('/api/restaurantes')
-def get_restaurantes(restaurante: str = Query(None)):
-    '''
-    Endpoint para ver o cardapio dos restaurantes
+    try:
+        response.raise_for_status()
+    except requests.exceptions.HTTPError as err:
+        return {"error": str(err)}
 
-    '''
-    url = 'https://guilhermeonrails.github.io/api-restaurantes/restaurantes.json'
-    response = requests.get(url)
+    return response.json()
 
-    if response.status_code == 200:
-        dados_json = response.json()
-        if restaurante is None:
-            return dados_json
-        
-        dados_restaurante = []
-        for item in dados_json:
-            if item['Company'] == restaurante:
-                dados_restaurante.append({
-                    'item': item['Item'],
-                    'price': item['price'],
-                    'description': item['description'],
-                })
-        return {'restaurante': restaurante, 'cardapio': dados_restaurante}
-    else: 
-        return{f'erro': f'{response.status_code} - {response.text}'}
+def main():
+    message = input('Type a message: ')
+    response = send_whatsapp_message('286165347924405', '11947316281', message)
+    print(response)
+
+if __name__ == '__main__':
+    main()
